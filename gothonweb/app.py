02154edb2c92e134ef609f8f8b96306e59fb8c04
ulfsjodin <sjodin.ulf@gmail.com>
2019-06-09 22:50:19 +0200

    starting to work on the web

---
+++ gothonweb/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, session, redirect, url_for, escape, request
+from flask import render_template
+from gothonweb import planisphere
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # This is used to "setup" the session with starting values.
+    session['room_name'] = planisphere.START
+    return redirect(url_for("game"))
+
+
+@app.route("/game", methods=['GET', 'POST'])
+def game():
+    room_name = session.get("room_name")
+    # return "Hollywood"
+    return room_name
+
+
+app.debug = True
+app.secret_key = "Höghults grustag"
+
+if __name__ == "__main__":
+    app.run()
